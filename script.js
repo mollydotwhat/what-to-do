d8fb0b851ce14d1ac83d2bf3975bdc5af8917a73
molly <molly.salzberger@gmail.com>
2023-11-19 18:50:35 -0600

    now i remember why i stopped--totally lost control of my variables. saving before clean-up & finish

--- script.js
+++ script.js
@@ -1,60 +1,81 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+//var rowEl = $('<tr>')
 $(function () {
-  var saveButton = $(".saveBtn");
- 
+  var today = dayjs().startOf('day');
+  var displayCurrent =  $('#currentDay');
+  displayCurrent.text(today);
+
+  // var agendaDisplay = $('#agenda-display');
+  // can I just do this programatically on page load? it would make more sense.
+  // var nineAm = $('.hour-nine');
+  // var tenAm = $('.hour-ten');
+  // var elevAM = $('.hour-eleven');
+  // var twelveNoon = $('.hour-twelve');
+  // var onePm = $('.hour-thirteen');
+  // var twoPm = $('.hour-fourteen');
+  // var threePm = $('.hour-fifteen');
+  // var fourPm = $('.hour-sixteen');
+  // var fivePm = $('.hour-seventeen');
+
+
+  // var taskText = ('.description') ;
+  // save time blocks to storage & make loadAgenda function? agenda starts empty but I still have to display the time
+  // var agenda = []
+
+  function makeTimeBlock(){
   //var timeBlock = document.createElement("div")??? or just put empty divs for each hour in html??
-    //DO IT THE SIMPLE WAY *FIRST* w/html in html file.
-      //maybe make global element to generate the time blocks, and then LATER fill them and analyze relative time??? but later.
-  //for each hour according to dayjs, so they can be compared to current time more easily?
+  
+  // need parseInt for hour? different way of identifying hour????
 
-  // timeBlock.textContent = anHour.itemText
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  
+  };
+  
+
+  var saveButton = $(".saveBtn");
   function saveToTimeBlock (event) {
     event.preventDefault();
     console.log("save item function");
-    // var timeBlock = {
-      // divId;
-      // paraText;
-      //definitely, definitely not right, but I do need an object like this to hold values for each time block
-    // };
-    var itemText = divIdparaVariable.val();
-    this.timeBlock.textContent = itemText;
-    //that syntax is garbage but it's the general idea    
-    //create object that contains values for time block
-    //save variables to local storage
-    //set element(s) of page to display this
+   var whatTask = $(this).siblings(".description").val();
+    var whatTime = $(this).parent().attr("id");
+    // simpler is better?
+    localStorage.setItem(whatTime, whatTask)
   };
 
-  //saveButton.on('click', saveToTimeBlock);
-
-  //show hour blocks containing what's been entered to local storage so far (should start empty)
-
-
-
+  saveButton.on('click', saveToTimeBlock);
+  
  
-  //make it specific to each block--should each block be an object with that property?
-  //
-
-
-
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  // make var timeBlock with parseInt div id
+  if (timeBlock.isBefore(dayjs().hour() )) {
+    rowEl.addClass('past');
+  } else if (timeBlock.isSame(dayjs().hour() )) {
+    rowEl.addClass('present');
+  } else if (timeBlock.isAfter(dayjs().hour() )){
+    rowEl.addClass('future');
+  }
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  function loadAgenda(){
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+  
+};
   //
   
   
@@ -70,7 +91,7 @@ $(function () {
     }
     displayTime();
 
-
+  
 });
 
 
